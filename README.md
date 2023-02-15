19c382b7f7c3360ef060cc40f9e9beea2ff9e3f3
Daniel Huidobro <elhui2@MacBook-Air-de-Daniel.local>
2023-02-15 01:24:49 -0600

    V 0.0.1 Se crea el repo

---
+++ README.md
@@ -0,0 +1,3 @@
+# animations V 0.0.1
+
+App para probar componentes en desktop
---
+++ lib/main.dart
@@ -0,0 +1,98 @@
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Animations'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.primaryContainer,
+        title: Text(widget.title),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text('Drawer Header'),
+            ),
+            ListTile(
+              title: const Text('Item 1'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: const Text('Item 2'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            CarouselSlider(
+              options: CarouselOptions(
+                height: 720.0,
+                autoPlay: true,
+                aspectRatio: 1.33,
+                enlargeCenterPage: true,
+              ),
+              items: [
+                "assets/slider-1.jpg",
+                "assets/slider-2.jpg",
+                "assets/slider-3.jpg",
+                "assets/slider-4.jpg",
+              ].map((i) {
+                return Builder(
+                  builder: (BuildContext context) {
+                    return Container(
+                      width: 1280,
+                      height: 720,
+                      child: Image.asset(i),
+                    );
+                  },
+                );
+              }).toList(),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
